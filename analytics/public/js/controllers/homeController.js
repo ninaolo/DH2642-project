2e4e13a767cb8a58f5c21d1bb5c74942985bebd4
Nina <ninaolo@kth.se>
2016-04-23 14:24:23 +0200

    Fix promise for homeController

--- analytics/public/js/controllers/homeController.js
+++ analytics/public/js/controllers/homeController.js
@@ -1,11 +1,12 @@
-analytics.controller("homeController", ['$scope', 'userService', function ($scope, userService) {
+analytics.controller("homeController", ['$scope', 'userService',
+    function ($scope, userService) {
 
         $scope.getLoggedIn = function () {
-            return userService.getLoggedIn().value;
+            return userService.getLoggedIn().$$state.value;
         };
 
         $scope.getLoggedUser = function () {
-            return userService.getLoggedUser().value;
+            return userService.getLoggedUser().$$state.value;
         };
 
     }]);
